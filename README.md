0f9d9b5aeaab605c8a45730cc2159122383f9b84
Jarkata <jarkata@foxmail.com>
2024-04-21 23:50:50 +0800

    add ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 go common tools
+
+## date 格式化及解析
+
+## snowflake id 生成
+
+```golang
+	nodeId := int64(1)
+	workId := int64(2)
+	id := tid.New(nodeId, workId)
+	slog.Info("", "ID", id)
+
+```
+
+## 工具类
--- tid/id_factory_test.go
+++ tid/id_factory_test.go
@@ -2,6 +2,7 @@ package tid_test
 
 import (
 	"fmt"
+	"log/slog"
 	"testing"
 
 	"github.com/jarkata/gotools/tid"
@@ -9,4 +10,8 @@ import (
 
 func TestGettime(t *testing.T) {
 	fmt.Printf("tid.GetInitTime(): %v\n", tid.GetInitTime())
+	nodeId := int64(1)
+	workId := int64(2)
+	id := tid.New(nodeId, workId)
+	slog.Info("", "", id)
 }

6d01838d46951ce16125039c2a9f0c42e3e20197
Jarkata <jarkata@foxmail.com>
2024-04-22 00:03:13 +0800

    feat:add LocalTime

--- date/local_date_time.go
+++ date/local_date_time.go
@@ -2,8 +2,6 @@ package date
 
 import (
 	"errors"
-	"fmt"
-	"log/slog"
 	"time"
 )
 
@@ -15,20 +13,23 @@ func New(t time.Time) LocalTime {
 	return LocalTime{t}
 }
 
+/*
+*
+格式化日期
+yyyy-MM-dd HH:mm:ss
+*/
 func (t LocalTime) MarshalJSON() ([]byte, error) {
 	//格式化秒
-	return []byte(fmt.Sprintf("\"%v\"", t.Time.Format("2006-01-02 15:04:05"))), nil
+	return []byte(FormatIsoDateTime(t.Time)), nil
 }
-func (t *LocalTime) UnmarshalJSON(data []byte) error {
 
+/** 解析JSON */
+func (t *LocalTime) UnmarshalJSON(data []byte) error {
 	if len(data) < 1 {
 		return errors.New("param invalid")
 	}
-	tmp := string(data[1 : len(data)-1])
-
-	value, err := time.ParseInLocation("2006-01-02T15:04:05.999Z", tmp, time.Local)
-	slog.Info("", "Value:", value, "ERR:", err)
-	slog.Info("", "格式化", value.Local().Format("2006-01-02 15:04:05"))
+	tmpValue := string(data[1 : len(data)-1])
+	value, err := ParseDateTime(tmpValue)
 	if err != nil {
 		return err
 	}

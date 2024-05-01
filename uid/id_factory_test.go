a051fddf5d77a28a59ae42e6e29de3cd87f343ae
Jarkata <jarkata@foxmail.com>
2024-05-02 06:39:07 +0800

    fix:id

--- uid/id_factory_test.go
+++ uid/id_factory_test.go
@@ -1,17 +1,17 @@
-package tid_test
+package uid_test
 
 import (
 	"fmt"
 	"log/slog"
 	"testing"
 
-	"github.com/jarkata/gotools/tid"
+	"github.com/jarkata/gotools/uid"
 )
 
 func TestGettime(t *testing.T) {
-	fmt.Printf("tid.GetInitTime(): %v\n", tid.GetInitTime())
+	fmt.Printf("tid.GetInitTime(): %v\n", uid.GetInitTime())
 	nodeId := uint64(1)
 	workId := uint64(2)
-	id := tid.New(nodeId, workId)
+	id := uid.New(nodeId, workId)
 	slog.Info("", "", id)
 }

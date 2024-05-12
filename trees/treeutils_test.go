02be3250f6eea068187d86ce2f3a9a0a62b985e4
Jarkata <jarkata@foxmail.com>
2024-05-12 17:37:08 +0800

    add trees

--- trees/treeutils_test.go
+++ trees/treeutils_test.go
@@ -1,9 +1,23 @@
 package trees_test
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/jarkata/gotools/trees"
 )
 
 func TestTree(t *testing.T) {
 
+	modal := []trees.TreeBO{
+		{Key: "root0", ParentKey: "root0", Label: "一级0", Ext: "testext"},
+		{Key: "root1", ParentKey: "root1", Label: "一级1", Ext: "testext"},
+		{Key: "root11", ParentKey: "root1", Label: "一级11", Ext: "testext"},
+		{Key: "root12", ParentKey: "root1", Label: "一级12", Ext: "testext"},
+		{Key: "root2", ParentKey: "root2", Label: "一级2", Ext: "testext2"},
+		{Key: "root21", ParentKey: "root2", Label: "二级21", Ext: "testext21"},
+		{Key: "root22", ParentKey: "root2", Label: "二级22", Ext: "testext22"},
+	}
+
+	fmt.Printf("trees.ToTree(modal): %v\n", trees.ToTree(modal))
 }
